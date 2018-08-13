d512271bae0a3a30b958438143437489739fae40
GuyTevet <guy.tvt@gmail.com>
2018-08-13 22:09:25 +0300

    evaluation ready

--- language_model_evaluation.py
+++ language_model_evaluation.py
@@ -13,8 +13,8 @@ import json
 # from tqdm import tqdm
 from sequence_gan import *
 
-SEQ_LENGTH = 200
-EXPERIMENT_NAME = 'dummy'
+SEQ_LENGTH = 200 # IF CHANGED - also delete Cache - aka 'text8-test.txt.npy'
+# EXPERIMENT_NAME = 'lm_only_320_0_0'
 
 def convergence_experiment(sess, tested_model, data_loader):
 
@@ -39,26 +39,21 @@ def convergence_experiment(sess, tested_model, data_loader):
 def language_model_evaluation(sess, tested_model, data_loader):
 
     data_loader.reset_pointer()
+    BPC_list = []
 
     for it in range(data_loader.num_batch):
         batch = data_loader.next_batch()
-        N = 1000 # hard coded for the meantime
-
-        # estimate probability vectors for batch
-        pred = np.zeros([BATCH_SIZE,SEQ_LENGTH,tested_model.num_emb],dtype=np.float32)
-        for i in range(N):
-            pred_one_hot, real_pred = tested_model.language_model_eval_step(sess, batch)
-            pred += real_pred # pred_one_hot
-        pred = pred / (N * 1.)
-        pred_flat = np.reshape(pred,[BATCH_SIZE * SEQ_LENGTH,tested_model.num_emb])
+        pred_one_hot, real_pred = tested_model.language_model_eval_step(sess, batch)
+        real_pred = np.clip(real_pred,1e-20,1)
+        pred_flat = np.reshape(real_pred,[BATCH_SIZE * SEQ_LENGTH,tested_model.num_emb])
         batch_flat = np.reshape(batch,[BATCH_SIZE * SEQ_LENGTH])
-        eps = 1e-20
 
         #calc bit per char
-        BPC = np.average([-np.log2(pred_flat[i,batch_flat[i]] + eps) for i in range(pred_flat.shape[0])])
-        print(str(BPC))
+        BPC = np.average([-np.log2(pred_flat[i,batch_flat[i]]) for i in range(pred_flat.shape[0])])
+        BPC_list.append(BPC)
 
-    return BPC
+
+    return np.average(BPC_list)
 
 def main():
     random.seed(SEED)
@@ -97,35 +92,37 @@ def main():
     sess = tf.Session(config=config)
     # sess.run(tf.global_variables_initializer())
 
-    print('#########################################################################')
-    print('loading model [%0s]...'%EXPERIMENT_NAME)
+    for exp_name in ['lm_only_0_0_0', 'lm_only_1_0_0', 'lm_only_120_0_0' , 'regular_120_50_200']:
+        print('#########################################################################')
+        print('loading model [%0s]...'%exp_name)
 
-    save_file = os.path.join('.', 'ckp', EXPERIMENT_NAME, EXPERIMENT_NAME)
-    reader = tf.train.NewCheckpointReader(save_file)
-    saved_shapes = reader.get_variable_to_shape_map()
-    var_names = sorted([(var.name, var.name.split(':')[0]) for var in tf.global_variables()
-                        if var.name.split(':')[0] in saved_shapes])
-    restore_vars = []
-    name2var = dict(list(zip([x.name.split(':')[0] for x in tf.global_variables()], tf.global_variables())))
-    with tf.variable_scope('', reuse=True):
-        for var_name, saved_var_name in var_names:
-            curr_var = name2var[saved_var_name]
-            var_shape = curr_var.get_shape().as_list()
-            if var_shape == saved_shapes[saved_var_name]:
-                restore_vars.append(curr_var)
-            else:
-                print(("Not loading: %s." % saved_var_name))
-    saver = tf.train.Saver(restore_vars)
-    print ("Restoring vars:")
-    print (restore_vars)
-    saver.restore(sess, save_file)
+        save_file = os.path.join('.', 'ckp', exp_name, exp_name)
+        reader = tf.train.NewCheckpointReader(save_file)
+        saved_shapes = reader.get_variable_to_shape_map()
+        var_names = sorted([(var.name, var.name.split(':')[0]) for var in tf.global_variables()
+                            if var.name.split(':')[0] in saved_shapes])
+        restore_vars = []
+        name2var = dict(list(zip([x.name.split(':')[0] for x in tf.global_variables()], tf.global_variables())))
+        with tf.variable_scope('', reuse=True):
+            for var_name, saved_var_name in var_names:
+                curr_var = name2var[saved_var_name]
+                var_shape = curr_var.get_shape().as_list()
+                if var_shape == saved_shapes[saved_var_name]:
+                    restore_vars.append(curr_var)
+                else:
+                    print(("Not loading: %s." % saved_var_name))
+        saver = tf.train.Saver(restore_vars)
+        print ("Restoring vars:")
+        print (restore_vars)
+        saver.restore(sess, save_file)
 
 
-    print('#########################################################################')
-    print('Start Language Model Evaluation...')
-    test_data_loader = Gen_Data_loader_text8(BATCH_SIZE,charmap,inv_charmap,SEQ_LENGTH)
-    test_data_loader.create_batches(real_data_test_file)
-    language_model_evaluation(sess,generator, test_data_loader)
+        print('#########################################################################')
+        print('Start Language Model Evaluation...')
+        test_data_loader = Gen_Data_loader_text8(BATCH_SIZE,charmap,inv_charmap,SEQ_LENGTH)
+        test_data_loader.create_batches(real_data_test_file)
+        BPC = language_model_evaluation(sess,generator, test_data_loader)
+        print("[%0s] BPC = %f"%(exp_name,BPC))
 
 if __name__ == '__main__':
     main()
